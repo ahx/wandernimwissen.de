2a2580d2e31915609b0368868fb5fbc7aead2902
Andreas Haller <andreashaller@gmail.com>
2010-06-16 04:48:20 +0200

    WIP submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
 $(function() {
+  $('form#query').bind('submit', function() {
+    var term = $('form#query input[name=term]').val();
+    $("form#query").hide();
+    $.ajax({
+      type: 'POST',
+      url: 'http://134.102.198.11:80/query',
+      data: { 'query': term },
+      success: function(data) {
+        $("form#query").show();
+        $("form#query .sent-info").show();
+      }
+    });
+    return false;
+  });
   
+  $('form#query submit-button').click(function() {
+   $('form#query').submit();
+  })
 });
